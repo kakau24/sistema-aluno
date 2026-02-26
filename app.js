511f22e9cc56a94c36694900a788ba76879df2e1
kakau24 <souza.rosa.kauan@escola.pr.gov.br>
2026-02-26 12:16:16 -0300

    app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const nomeAluno = "KAUAN";// String
+const idade = "16";// Number
+const matriculado = true; //Boolean
+
+console.log(`Aluno: ${nome}, Idade: ${idade}, Matriculado: ${matriculado}`);s
